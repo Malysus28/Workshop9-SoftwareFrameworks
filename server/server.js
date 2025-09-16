3d43215c863c6cbf11c9b8f48d8af528d31f311e
MaleesA28 <mariyammalys@gmail.com>
2025-09-16 21:14:45 +1000

    routes added to the server.js

--- server/server.js
+++ server/server.js
@@ -12,6 +12,55 @@ app.get("/", (req, res) => {
   res.send("server/API is running");
 });
 
+// note: api call is when a client (angular) makes a request to a specific route in express server. express route can catch the req and talk to mongodb and then it can send a response back.
+
+// get all products
+app.get("/products", (req, res) => {
+  connectToMyDB().then(({ products, client }) => {
+    products
+      .find({})
+      .toArray()
+      .then((docs) => {
+        // this is the sending back the response to the angualar
+        res.json(docs);
+        client.close();
+      });
+  });
+});
+
+//post add stuff
+app.post("/products", (req, res) => {
+  connectToMyDB().then(({ products, client }) => {
+    products.insertOne(req.body).then(() => {
+      res.json({ message: "Product added" });
+      client.close();
+    });
+  });
+});
+
+// put update stuff
+app.put("/products/:id", (req, res) => {
+  connectToMyDB().then(({ products, client }) => {
+    products
+      .updateOne({ id: Number(req.params.id) }, { $set: req.body })
+      .then((pds) => {
+        res.json({ matched: pds.matchedCount, modified: pds.modifiedCount });
+        client.close();
+      });
+  });
+});
+
+// delete stuff
+app.delete("/products/:id", (req, res) => {
+  connectToMyDB().then(({ products, client }) => {
+    products.deleteOne({ id: Number(req.params.id) }).then((pds) => {
+      res.json({ deleted: pds.deletedCount });
+      client.close();
+    });
+  });
+});
+
+// start server
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
