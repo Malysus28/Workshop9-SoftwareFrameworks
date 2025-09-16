370dd3e36dc66e5278839b89b7a919550b6a230b
MaleesA28 <mariyammalys@gmail.com>
2025-09-17 08:04:38 +1000

    comments added to products-page.ts, so i can refer to my notes

--- angularProj/src/app/component/product-page/product-page.ts
+++ angularProj/src/app/component/product-page/product-page.ts
@@ -10,16 +10,20 @@ import { ProductsAPI, Product } from '../../products-api';
   templateUrl: './product-page.html',
 })
 export class ProductPage {
+  // this is my empty array that holds the products.
   items: Product[] = [];
   constructor(private productsAPI: ProductsAPI) {}
   ngOnInit() {
+    // to fetch from the back end.
     this.load();
   }
+
+  // call my productsAPI service to get all the products from backend. localhost:whatever/products. need to use subscribe because httpClient.get() returns an observable. when data is recived 'dis' is my array with the products. and them we need to display them
 
   load() {
     this.productsAPI.getAll().subscribe((dis) => (this.items = dis));
   }
-
+  // delete the product, then reload the list again so it refreshes and updates
   remove(id: number) {
     this.productsAPI.remove(id).subscribe(() => this.load());
   }
